8a5c0c09fbc725fed44a7a44dce539eb52e589bc
Esther <estshwang@gmail.com>
2019-06-22 12:39:39 -0500

    Updates

---
+++ precache-manifest.c37456edcec979a3ead47f28ade3a3e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c64ad31c6a3e078deb90417895e4a5",
+    "url": "/click-game2/index.html"
+  },
+  {
+    "revision": "f51b3cfea4d53790153e",
+    "url": "/click-game2/static/css/main.6ca45a80.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/click-game2/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "f51b3cfea4d53790153e",
+    "url": "/click-game2/static/js/main.e13b564f.chunk.js"
+  },
+  {
+    "revision": "fca24445bbea8e958689",
+    "url": "/click-game2/static/js/runtime~main.cf87ae46.js"
+  }
+]);
